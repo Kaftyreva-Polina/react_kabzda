718e57fe2e19b7efcdfc77febae2bd83b6fc0964
KaftyrevaPolina <pkaft.13.8@gmail.com>
2023-03-19 05:58:19 +0300

    6-7 useState, uncontrolled components

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,40 @@ import React from "react";
 import "./App.css";
 import {Accordion} from "./components/Accordion/Accordion";
 import {Rating} from "./components/Rating/Rating";
+import OnOff from "./components/OnOff/OnOff";
+import {UncontrolledAccordion} from "./components/UncontrolledAccordion/UncontrolledAccordion";
+import {UncontrolledRating} from "./components/UncontrolledRating/UncontrolledRating";
+
 
 function App() {
     console.log("App rendering")
     // debugger
     return (
         <div>
-            <PageTitle title={"This is APP component"}/>
-            <PageTitle title={"My friends"}/>
-            Article 1
+            <OnOff />
+            <OnOff />
+
+            <UncontrolledAccordion titleValue={"Menu"}/>
+            <UncontrolledRating />
+
+            <Accordion titleValue={"Menu"} collapsed={false}/>
             <Rating value={3}/>
-            <Accordion titleValue={"Menu"} collapsed={true}/>
-            <Accordion titleValue={"Users"} collapsed={false}/>
-            Article 2
-            <Rating value={0}/>
-            <Rating value={1}/>
-            <Rating value={2}/>
-            <Rating value={3}/>
-            <Rating value={4}/>
-            <Rating value={5}/>
+
+            {/*<OnOff on={false}/>*/}
+            {/*<OnOff on={true}/>*/}
+            {/*<PageTitle title={"This is APP component"}/>*/}
+            {/*<PageTitle title={"My friends"}/>*/}
+            {/*Article 1*/}
+            {/*<Rating value={3}/>*/}
+            {/*<Accordion titleValue={"Menu"} collapsed={true}/>*/}
+            {/*<Accordion titleValue={"Users"} collapsed={false}/>*/}
+            {/*Article 2*/}
+            {/*<Rating value={0}/>*/}
+            {/*<Rating value={1}/>*/}
+            {/*<Rating value={2}/>*/}
+            {/*<Rating value={3}/>*/}
+            {/*<Rating value={4}/>*/}
+            {/*<Rating value={5}/>*/}
         </div>
     );
 }
